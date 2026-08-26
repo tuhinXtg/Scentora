7778bbfd47f008d05e2ede5cedfa12caaff274b6
Namul Alam Tuhin <natuhin02@gmail.com>
2026-08-26 17:34:36 +0600

    made the pages we will need so far

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,26 @@
-import Navbar from "./components/Navbar"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Products from './pages/Products'
+import ProductDetails from './pages/ProductDetails'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Checkout from './pages/Checkout'
 
 function App() {
   return (
-
-    <>
-      <Navbar/>
-
-      <main>
-        <h2>Welcome to Scentora</h2>
-      </main>
-    </>
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/ProductDetails.tsx
@@ -0,0 +1,4 @@
+export default function ProductDetails() {
+  return <div>Product Details Page</div>;
+}
+
